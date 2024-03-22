440006527a058f318990202c23e5a5df38c1458c
SyndielAlo <123524195+SyndielAlo@users.noreply.github.com>
2024-03-23 00:47:31 +0800

    Update setup.js

--- src/firebase/setup.js
+++ src/firebase/setup.js
@@ -9,14 +9,7 @@ import {getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDARudR_zkIxCJl2HkhomivoOhMk_XO_fQ",
-    authDomain: "visualgreen-519e5.firebaseapp.com",
-    databaseURL: "https://visualgreen-519e5-default-rtdb.firebaseio.com",
-    projectId: "visualgreen-519e5",
-    storageBucket: "visualgreen-519e5.appspot.com",
-    messagingSenderId: "1025073099068",
-    appId: "1:1025073099068:web:59f27abb80b490da80ae39",
-    measurementId: "G-7B4XH1JBMR"
+   
   };
   
   
